653f3b0198998ee1060783d80f7adbd31f10d85f
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-06-22 17:36:00 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,50 @@
-# Code your instances here
+# Code your instances  
+class GardenGnome
+  def name(gnome_name)
+    return @name
+  end
+  
+  def name=(gnome_name)
+    @name = gnome_name
+  end
+  
+  def age(gnome_age)
+    return @age
+  end
+  
+  def age=(gnome_age)
+    @age = gnome_age
+  end
+  
+  def gluten_allergy
+    return @gluten_allergy
+  end
+  
+  def gluten_allergy=(gluten_allergy)
+    @gluten_allergy = gluten_allergy
+  end
+  
+  def initialize("evil", hat_color)
+    @personality = "evil"
+    def hat_color(color)
+      return @hat_color
+    end
+    
+    def hat_color=(color)
+      @hat_color = color
+    end
+    
+  def gnaw
+    return "Gnawing on a tree!!!"
+  end
+  
+  def shout
+    return "GNARLY!!!"
+  end
+  
+  def introduce_self
+    return "Hello humans, my name is @gnome_name, I am @gnome_age years old, and you'll rue the day you crossed me!"
+  end
+  end
+  
+end
